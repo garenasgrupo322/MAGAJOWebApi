3ae2df47f555059014a3fe6f138f65667f5f5dce
Gad Arenas <garenas.grupoempresarial322@gmail.com>
2019-05-21 12:33:27 -0500

    Explosion

--- MAGAJOWebApi/Controllers/CatalogController.cs
+++ MAGAJOWebApi/Controllers/CatalogController.cs
@@ -45,8 +45,8 @@ namespace MAGAJOWebApi.Controllers {
         }
 
         [HttpPost]
-        [Route("GetData")]
-        public ActionResult Post([FromBody] EntityData parametros)
+        [Route("ObtieneExplosionDatos")]
+        public ActionResult Post([FromBody] string filtro)
         {
             string json = string.Empty;
             List<Parameters> parameters = new List<Parameters>();
@@ -137,6 +137,112 @@ namespace MAGAJOWebApi.Controllers {
             {
                 json = utilities.callSP("MGJENT_GET_ENTITY_DATA", parameters, "@IDataOutput");
             } 
+            catch (Exception ex)
+            {
+                return BadRequest(new
+                {
+                    success = false,
+                    message = ex.Message
+                });
+            }
+
+            return Ok(json);
+        }
+
+        [HttpPost]
+        [Route("GetData")]
+        public ActionResult Post([FromBody] EntityData parametros)
+        {
+            string json = string.Empty;
+            List<Parameters> parameters = new List<Parameters>();
+
+            Parameters parameter = new Parameters();
+            parameter.Name = "@IEntityID";
+            parameter.Direction = ParameterDirection.Input;
+            parameter.Type = SqlDbType.VarChar;
+            parameter.Value = parametros.IEntityID;
+            parameter.Size = -1;
+            parameters.Add(parameter);
+
+            parameter = new Parameters();
+            parameter.Name = "@Itoken";
+            parameter.Direction = ParameterDirection.Input;
+            parameter.Type = SqlDbType.VarChar;
+            parameter.Value = parametros.Itoken;
+            parameter.Size = -1;
+            parameters.Add(parameter);
+
+            parameter = new Parameters();
+            parameter.Name = "@FilterData";
+            parameter.Direction = ParameterDirection.Input;
+            parameter.Type = SqlDbType.VarChar;
+            if (parametros.FilterData != null)
+            {
+                var jsonFilter = JsonConvert.SerializeObject(parametros.FilterData);
+                parameter.Value = jsonFilter;
+            }
+            else
+            {
+                parameter.Value = DBNull.Value;
+            }
+            parameter.Size = -1;
+            parameters.Add(parameter);
+
+            parameter = new Parameters();
+            parameter.Name = "@SortData";
+            parameter.Direction = ParameterDirection.Input;
+            parameter.Type = SqlDbType.VarChar;
+            if (!string.IsNullOrEmpty(parametros.SortData))
+                parameter.Value = parametros.SortData;
+            else
+                parameter.Value = DBNull.Value;
+            parameter.Size = -1;
+            parameters.Add(parameter);
+
+            parameter = new Parameters();
+            parameter.Name = "@QueryLimits";
+            parameter.Direction = ParameterDirection.Input;
+            parameter.Type = SqlDbType.VarChar;
+            if (!string.IsNullOrEmpty(parametros.QueryLimits))
+                parameter.Value = parametros.QueryLimits;
+            else
+                parameter.Value = DBNull.Value;
+            parameter.Size = -1;
+            parameters.Add(parameter);
+
+            parameter = new Parameters();
+            parameter.Name = "@ColumnData";
+            parameter.Direction = ParameterDirection.Input;
+            parameter.Type = SqlDbType.VarChar;
+            if (!string.IsNullOrEmpty(parametros.ColumnData))
+                parameter.Value = parametros.ColumnData;
+            else
+                parameter.Value = DBNull.Value;
+            parameter.Size = -1;
+            parameters.Add(parameter);
+
+            /*parameter = new Parameters();
+            parameter.Name = "@MGJAPP_ID";
+            parameter.Direction = ParameterDirection.Input;
+            parameter.Type = SqlDbType.Char;
+            parameter.Value = parametros.MGJAPP_ID;
+            parameter.Size = 10;
+            parameters.Add(parameter);*/
+
+            parameter = new Parameters();
+            parameter.Name = "@IDataOutput";
+            parameter.Direction = ParameterDirection.InputOutput;
+            parameter.Type = SqlDbType.VarChar;
+            parameter.Value = string.Empty;
+            parameter.Size = -1;
+            parameters.Add(parameter);
+
+            Utilities utilities = new Utilities(_configuration);
+
+            try
+            {
+                json = utilities.callSP("MGJENT_GET_ENTITY_DATA", parameters, "@IDataOutput");
+            }
             catch (Exception ex)
             {
                 return BadRequest(new
